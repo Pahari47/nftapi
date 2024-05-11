aecc5288fe6dc46cf610b08d5ce18e1e9f4ca57d
Pahari47 <nachiketapahari00@gmail.com>
2024-05-11 18:54:16 +0530

    organized

--- app.js
+++ app.js
@@ -1,112 +1,26 @@
-const fs = require("fs")
 const express = require("express");
+const morgan = require("morgan");
+
+const nftsRouter = require("./routes/nftsRoute");
+const usersRouter = require("./routes/usersRoute");
 
 const app = express();
 app.use(express.json())
+app.use(morgan("dev"))
 
-const nfts = JSON.parse(
-    fs.readFileSync(`${__dirname}/nft-data/data/nft-simple.json`)
-);
-
-// GET METHOD
-
-const getAllNfts = (req, res) => {
-    res.status(200).json({
-        status: "success",
-        results: nfts.length,
-        data: {
-            nfts,
-        },
-    })
-}
-
-
-// POST METHOD
-
-const createNFT = (req, res) => {
-
-    const newId = nfts[nfts.length - 1].id + 1;
-    const newNFTs = Object.assign({ id: newId }, req.body)
-
-    nfts.push(newNFTs);
-
-    fs.writeFile(`${__dirname}/nft-data/data/nft-simple.json`, JSON.stringify(nfts), err => {
-        res.status(201).json({
-            status: "success",
-            nft: newNFTs,
-        })
-    })
-}
-
-
-// GET SINGLE NFT
-
-const getSingleNFT = (req, res)=> {
-    
-    const id = req.params.id * 1;
-    const nft = nfts.find((el) => (el.id === id));
-
-    if(!nft){
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid ID"
-        })
-    }
-
-    res.status(200).json({
-        status: "success",
-        data: {
-            nft,
-        },
-    })
-}
-
-// PATCH METHOD
-
-const updateNFT = (req, res)=> {
-
-    if(req. params.id * 1 > nfts.length){
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid ID"
-        })
-    }
-    res.status(200).json({
-        status: "success",
-        data: {
-            nft: "Updating nft"
-        }
-    })
-}
-
-// DELETE METHOD
-
-const deleteNFT = (req, res)=> {
-
-    if(req. params.id * 1 > nfts.length){
-        return res.status(404).json({
-            status: "fail",
-            message: "Invalid ID"
-        })
-    }
-    res.status(204).json({
-        status: "success",
-        data: null,
-    })
-}
-
-
-// app.get('/api/v1/nfts', getAllNfts)
-// app.post("/api/v1/nfts", createNFT)
-// app.get("/api/v1/nfts/:id", getSingleNFT)
-// app.patch("/api/v1/nfts/:id", updateNFT)
-// app.delete("/api/v1/nfts/:id", deleteNFT)
-
-app.route("/api/v1/nfts").get(getAllNfts).post(createNFT);
-
-app.route("/api/v1/nfts/:id").get(getSingleNFT).patch(updateNFT).delete(deleteNFT);
-
-const port = 4000;
-app.listen(port, () => {
-    console.log(`App running on port ${port}....`);
+// CUSTOM MIDDLE WARE
+app.use((req, res, next)=> {
+    console.log("Hey i am from middleware function 👋");
+    next();
 });
+
+app.use((req, res, next)=> {
+    req.requestTime = new Date().toISOString();
+    next();
+})
+
+
+app.use("/api/v1/nfts", nftsRouter);
+app.use("/api/v1/users", usersRouter);
+
+module.exports = app;
