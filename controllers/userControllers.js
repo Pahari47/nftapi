7839e8e06b14bcbe73499093ee217d5da3ab7035
Pahari47 <nachiketapahari00@gmail.com>
2024-05-11 18:54:47 +0530

    organized

---
+++ controllers/userControllers.js
@@ -0,0 +1,30 @@
+/// -----USERS
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        message: "Internal server error",
+    });
+}
+
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        message: "Internal server error",
+    });
+}
+
+exports.getSingleUser = (req, res) => {
+    res.status(500).json({
+        message: "Internal server error",
+    });
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        message: "Internal server error",
+    });
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        message: "Internal server error",
+    });
+}
---
+++ routes/nftsRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const nftControllers = require("./../controllers/nftControllers");
+// const { getAllNfts, createNFT, getSingleNFT, updateNFT, deleteNFT } = require("./../controllers/nftControllers");
+
+
+const router = express.Router();
+
+
+// ROUTER NFTs
+router.route("/").get(nftControllers.getAllNfts).post(nftControllers.createNFT);
+
+router.route("/:id").get(nftControllers.getSingleNFT).patch(nftControllers.updateNFT).delete(nftControllers.deleteNFT);
+
+module.exports = router;
