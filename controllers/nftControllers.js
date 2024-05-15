9725f5d21ed7294a10d9583582ed0a17507a9124
Pahari47 <nachiketapahari00@gmail.com>
2024-05-16 00:46:33 +0530

    Adding Mongose pipleline & Monthly plane

--- controllers/nftControllers.js
+++ controllers/nftControllers.js
@@ -1,8 +1,9 @@
 
 
 const NFT = require("./../models/nftModel");
+const APIFeatures = require("./../Utils/apiFeatures");
 
-exports.aliasTopNFTs = (req, res, next)=>{
+exports.aliasTopNFTs = (req, res, next) => {
     req.query.limit = '5',
     req.query.sort = "-ratingsAverage,price";
     req.query.fields = "name,price,ratingsAverage,difficulty";
@@ -12,63 +13,10 @@ exports.aliasTopNFTs = (req, res, next)=>{
 
 exports.getAllNfts = async (req, res) => {
     try {
-        // BUILD QUERY  
 
-        const queryObj = {...req.query}
-        const excludedFields = ["page", "sort", "limit", "fields"];
-        excludedFields.forEach((eL) => delete queryObj[eL]);
+        const features = new APIFeatures(NFT.find(), req.query).filter().sort().limitFields().pagination();
+        const nfts = await features.query;
 
-        // console.log(req.query, queryObj);
-
-
-        // ADVANCE FILTERING QUERY
-        let queryStr = JSON.stringify(queryObj);
-        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
-        // console.log(JSON.parse(queryStr));
-
-        let query = NFT.find(JSON.parse(queryStr));
-        // {difficulty: "easy", duration: {$gte: 5}}
-        // { difficulty: 'easy', duration: { gte: '5' } }
-        // { difficulty: 'easy', duration: { '$gte': '5' } }
-
-        // SORTING METHOD
-        if (req.query.sort){
-            const sortBy = req.query.sort.split(',').join(" ");
-            console.log(sortBy);
-            query = query.sort(sortBy);
-        } else{
-            query = query.sort("-createdAt");
-        }
-
-        // FIELD LIMITS
-        if(req.query.fields){
-            const fields = req.query.fields.split(",").join(' ');
-            query = query.select(fields);
-        } else{
-            query = query.select("-__v");
-        }
-
-        // PAGINATIONS FUNCTION
-        const page = req.query.page * 1 || 1
-        const limit = req.query.limit * 1 || 10
-        const skip = (page - 1) * limit;
-
-        query = query.skip(skip).limit(limit);
-
-        if(req.query.page){
-            const newNFTs = await NFT.countDocuments();
-            if (skip >= newNFTs) throw new Error("This page doesn't exist");
-        }
-
-        const nfts = await query;
-
-        // console.log(req.query);
-        // const nfts = await NFT.find({
-        //     difficulty: "easy",
-        //     duration: 5
-        // });
-
-        // const nfts = await NFT.find().where('duration').equals(5).where("difficulty").equals("easy");
 
         // SEND QUERY
         res.status(200).json({
@@ -172,4 +120,104 @@ exports.deleteNFT = async (req, res) => {
         })
     }
 
+}
+
+
+// Aggregation Pipeline
+
+exports.getNFTsStats = async (req, res)=> {
+    try {
+       
+        const stats = await NFT.aggregate([
+            {
+                $match: {ratingsAverage: { $gte: 4.5 }},
+            },
+            {
+                $group: {
+                    _id: {$toUpper: "$difficulty"},
+                    numNFT: {$sum: 1},
+                    numRatings: {$sum: "ratingsQuantity"},
+                    avgRating: {$avg: "$ratingsAverage"},
+                    avgPrice: {$avg: "$price"},
+                    minPrice: {$min: "$price"},
+                    maxPrice: {$max: "$price"},
+                },
+            },
+            {
+                $sort: {avgRating: 1},
+            },{
+                $match: {
+                    _id: {$ne: "EASY"},
+                },
+            },
+        ]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                stats,
+            },
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: "error",
+        })
+    }
+}
+
+// Calculating Number Of NFT Create In The Month Or Monthly Plan
+
+exports.getMonthlyPlan = async (req, res) => {
+    try {
+        const year = req.params.year * 1;
+        const plan = await NFT.aggregate([
+            {
+                $unwind: "$startDates",
+            },
+            {
+                $match: {
+                    startDates: {
+                        $gte: new Date(`${year}-01-01`),
+                        $lte: new Date(`${year}-12-31`),
+                    }
+                },
+            },
+            {
+                $group: {
+                    _id: {$month: "$startDates"},
+                    numNFTStarts: {$sum: 1},
+                    nfts: {$push: "$name"},
+                },
+            },
+            {
+                $addFields: {
+                    month: "$_id"
+                },
+            },
+            {
+                $project:{
+                    _id: 0,
+                },
+            },
+            {
+                $sort:{
+                    numNFTStarts: -1
+                },
+            },
+            {
+                $limit: 6,
+            }
+        ]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                plan,
+            },
+        });
+    } catch (error) {
+        res.status(404).json({
+            status: "fail",
+            message: "error",
+        })
+    }
 }
--- routes/nftsRoute.js
+++ routes/nftsRoute.js
@@ -10,6 +10,12 @@ const router = express.Router();
 // TOP 5 NFTs BY PRICE
 router.route('/top-5-nfts').get(nftControllers.aliasTopNFTs, nftControllers.getAllNfts)
 
+// STATS ROUTE
+router.route("/nfts-stats").get(nftControllers.getNFTsStats);
+
+// GET MONTHLY PLAN
+router.route("/monthly-plan/:year").get(nftControllers.getMonthlyPlan);
+
 // ROUTER NFTs
 router.route("/").get(nftControllers.getAllNfts)
 // .post(nftControllers.checkBody, nftControllers.createNFT);
